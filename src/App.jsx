348987b29e996740acff5cf59ab716a224271395
psiwlf <sroot@Ss-MacBook-Pro.local>
2022-06-12 12:27:40 +0500

    feat: dark theme

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,15 @@
-import { useState } from 'react';
+import { Button, useColorMode } from '@chakra-ui/react';
+import { FaMoon, FaSun } from 'react-icons/fa';
 import './App.css';
-import { useArtistFetch } from './useArtistFetch';
 import SearchView from './views/SearchView';
 
 function App() {
+  const { colorMode, toggleColorMode } = useColorMode();
   return (
     <div className="App">
+      <Button onClick={toggleColorMode}>
+        {colorMode === 'light' ? <FaMoon /> : <FaSun />}
+      </Button>
       <SearchView />
     </div>
   );
